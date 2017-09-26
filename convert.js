e4bd96444d573b600792c78cec37dfc306c0ac2a
zhhee1 <zhhee1@student.monash.edu>
2017-09-26 18:21:44 +1000

    w

--- convert.js
+++ convert.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let PDFParser = require("pdf2json");
 
 let source = ["ENG1001", "ENG1002", "ENG1003", "ENG1005", "ENG1060", "ENG1090",
-    "ENG2005", "ENG2801", "ECE2072", "ECE2111", "ECE2191", "ECE3091", "ECE3121",
-    "ECE4032", "ECE4042", "ECE4043", "ECE4053", "ECE4076", "ECE4087", "ECE4094",
-    "ECE4095", "ECE4122", "ECE4808", "ECE5882", "ECE5884", "ECE5886","TRC2001"];
+    "ENG2005", "ENG2801", "ECE2072", "ECE2111", "ECE2191", "ECE3051", "ECE3062",
+    "ECE3091", "ECE3121", "ECE4032", "ECE4042", "ECE4043", "ECE4053", "ECE4076",
+    "ECE4087", "ECE4094", "ECE4095", "ECE4122","MEC2407", "MEC3458", "MTH1030", "TRC3600"];
 
 for (i = 0; i < source.length; i++) {
     let read = "./PDF/"+source[i]+"_Semester2(S2-01)_2017.pdf";
